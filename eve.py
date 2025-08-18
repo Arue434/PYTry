debf8b36c1d5503dbdd427177f9a56ccbe015926
ArKa <91108604+Arue434@users.noreply.github.com>
2025-08-18 12:18:24 +0700

    first

---
+++ eve.py
@@ -0,0 +1,17 @@
+print ("Karael")
+
+a = 10 
+b = 20
+
+print (a)
+print (b)
+
+print (a + b) 
+print (a - b)
+
+print ("celcius to fahrenheit")
+c = 5
+
+f = (c * 9/5) + 32
+print ("hasilnya adalah :") 
+print (f)
