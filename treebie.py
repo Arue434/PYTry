c20e617ae92cbfdb1d1d48fae68dc2bdb555e441
ArKa <91108604+Arue434@users.noreply.github.com>
2025-08-18 13:59:31 +0700

    Update treebie.py

--- treebie.py
+++ treebie.py
@@ -9,23 +9,7 @@ def cabangan(a):
         return "bilangan negatif"
     else:
         return "bilangan nol"
-    
-    # bilangan = 5
-    # hasil = cabangan(bilangan) 
-    # print(f"a {bilangan} adalah {hasil}") #bilangan positif
-
-    # bilangan = -5
-    # hasil = cabangan(bilangan) 
-    # print(f"a {bilangan} adalah {hasil}") #bilangan negatif
-
-    # bilangan = 0
-    # hasil = cabangan(bilangan) 
-    # print(f"a {bilangan} adalah {hasil}") #bilangan nol
-
-# a = int(input("Masukkan bilangan: "))
-# hasil = cabangan(a)
-# print(f"Bilangan yang dimasukkan adalah {hasil}") 
-
+        
 while True:
     text = input ("Masukkan bilangan (atau ketik 'exit' untuk keluar): ")
     if text.lower() == 'exit':
@@ -39,4 +23,5 @@ while True:
 
     else:
         print("Input tidak valid.")
+
 
