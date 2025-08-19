297161b5265fb17739d659fb2db6f75acaba110d
ArKa <91108604+Arue434@users.noreply.github.com>
2025-08-19 13:24:10 +0700

    list dinamis dan matriks

---
+++ ester.py
@@ -0,0 +1,27 @@
+oreList = []
+for i in range(1, 4):   #matriks
+    row = []
+    for j in range(1, 4):
+        row.append(i * j)
+    oreList.append(row)
+print(oreList)
+
+oreList = []
+for i in range(1, 10):
+    oreList.append(i)
+print(oreList)
+
+# --------------------------------------------------------------------------------------
+print("Daftar dinamis, ketik 'done' untuk selesai")
+lister = []
+
+while True:
+    text = input ("Masukkan item anda: ")
+    if text.lower() == 'done':
+            print("pesanan selesai, terima kasih!")
+            break
+    lister.append(text)
+
+print("Daftar item anda:")
+for i, item in enumerate(lister, start=1):
+    print(f"{i}. {item}")
